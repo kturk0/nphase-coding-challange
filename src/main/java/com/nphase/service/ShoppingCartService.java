f90f18be9b5d7d4db86b52d7a4e30dce67d5984e
kturk0 <46371512+kturk0@users.noreply.github.com>
2023-07-26 20:27:28 +0200

    tasks completed

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,52 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.HashMap;
 
 public class ShoppingCartService {
+    public final double productDiscount;
+    public final int productThreshold;
+    public final double categoryDiscount;
+    public final int categoryThreshold;
+
+    public ShoppingCartService() {
+        productDiscount = 0.1;
+        productThreshold = 3;
+        categoryDiscount = 0.1;
+        categoryThreshold = 3;
+    }
+
+    public ShoppingCartService(double productDiscount, int productThreshold, double categoryDiscount, int categoryThreshold) {
+        this.productDiscount = productDiscount;
+        this.productThreshold = productThreshold;
+        this.categoryDiscount = categoryDiscount;
+        this.categoryThreshold = categoryThreshold;
+    }
 
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
+        HashMap<String, Integer> categoryMap = new HashMap<String,Integer>();
+        shoppingCart.getProducts().forEach(
+                product -> categoryMap.put(product.getCategory(), categoryMap.getOrDefault(product.getCategory(),0) + product.getQuantity()));
+
         return shoppingCart.getProducts()
                 .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
+                .map(product -> calculatePrice(product,categoryMap))
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    public BigDecimal calculatePrice(Product product, HashMap<String,Integer> categoryMap) {
+        BigDecimal price = product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity()));
+        BigDecimal discount = BigDecimal.valueOf(1);
+        if(product.getQuantity() > productThreshold)
+            discount = discount.subtract(BigDecimal.valueOf(productDiscount));
+        if(categoryMap.get(product.getCategory()) > categoryThreshold)
+            discount = discount.subtract(BigDecimal.valueOf(categoryDiscount));
+        return price.multiply(discount).round(new MathContext(4, RoundingMode.CEILING)).stripTrailingZeros();
     }
 }
--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -14,10 +14,10 @@ public class ShoppingCartServiceTest {
     private final ShoppingCartService service = new ShoppingCartService();
 
     @Test
-    public void calculatesPrice()  {
+    public void calculatesPrice() {
         ShoppingCart cart = new ShoppingCart(Arrays.asList(
-                new Product("Tea", BigDecimal.valueOf(5.0), 2),
-                new Product("Coffee", BigDecimal.valueOf(6.5), 1)
+                new Product("Tea", BigDecimal.valueOf(5.0), 2, "drinks"),
+                new Product("Coffee", BigDecimal.valueOf(6.5), 1, "drinks")
         ));
 
         BigDecimal result = service.calculateTotalPrice(cart);
@@ -25,4 +25,44 @@ public class ShoppingCartServiceTest {
         Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
     }
 
+    @Test
+    public void appliesDiscounts() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.0), 5, "drinks"),
+                new Product("Cheese", BigDecimal.valueOf(3.5), 3, "food")
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        Assertions.assertEquals(BigDecimal.valueOf(30.5), result);
+    }
+
+    // testing task 3
+    @Test
+    public void appliesCategoryDiscount() {
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.3), 2, "drinks"),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 2, "drinks"),
+                new Product("Cheese", BigDecimal.valueOf(8), 2, "food")
+        ));
+
+        BigDecimal result = service.calculateTotalPrice(cart);
+
+        Assertions.assertEquals(BigDecimal.valueOf(31.84), result);
+    }
+
+    // testing task 4
+    @Test
+    public void customDiscountsWorking() {
+        ShoppingCartService customService = new ShoppingCartService(0.2,5,0.1,10);
+        ShoppingCart cart = new ShoppingCart(Arrays.asList(
+                new Product("Tea", BigDecimal.valueOf(5.4), 6, "drinks"),
+                new Product("Coffee", BigDecimal.valueOf(4.2), 4, "drinks"),
+                new Product("Cheese", BigDecimal.valueOf(10), 12, "food")
+        ));
+
+        BigDecimal result = customService.calculateTotalPrice(cart);
+
+        Assertions.assertEquals(BigDecimal.valueOf(126.72), result);
+    }
 }
